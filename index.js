38af4f9bbb084f3c3974bfaaa847d11a9933568f
juheedhone <juheedhone12@gmail.com>
2025-05-28 14:42:38 +0530

    All exercise From Function.

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const person = {
-  firstName: "juhee",
-  lastName: "dhone",
+// Exercise sum of argument in array
 
-  set fullName(value) {
-    if (typeof value !== "string") throw new Error("not a string ");
+// function sum(...args) {
+//   if (args.length === 1 && Array.isArray(args[0])) args = [...args[0]];
+//   return args.reduce((a, b) => a + b);
+// }
 
-    const parts = value.split(" ");
-    if (parts.length !== 2) throw new Error("enter first and last name");
+// console.log(sum([1, 2, 3, 4]));
 
-    this.firstName = parts[0];
-    this.lastName = parts[1];
-  },
-};
+//Exercise area of circle
 
+// const circle = {
+//   radius: 2,
+//   get area() {
+//     return 3.15 * this.radius * this.radius;
+//   },
+// };
+
+// console.log(circle.area);
+
+// Exercise Error Handling
+
+const numbers = [1, 2, 3, 4, 5];
 try {
-  person.fullName = null;
-} 
-catch (e) {
-  alert(e);
+  const count = countOccurrence(numbers, 1);
+  console.log(count);
+} catch (e) {
+  console.log("error", e);
 }
 
-console.log(person);
+function countOccurrence(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error("value is not number");
+  return array.reduce((acc, current) => {
+    const occur = current === searchElement ? 1 : 0;
+    return acc + occur;
+  }, 0);
+}
